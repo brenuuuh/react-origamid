648e7b032fcd61832b3e37b190969f01934257bb
Breno Figueiredo <breno.figueiredo@sovos.com>
2023-07-14 16:18:44 +0100

    Include feed modal behaviors

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles from "./FeedModal.module.css";
+import useFecth from "../../Hooks/useFetch";
+import { PHOTO_GET } from "../../api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import PhotoContent from "../Photo/PhotoContent";
 
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFecth();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
+
+  return (
+    <section className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </section>
+  );
 };
 
 export default FeedModal;
